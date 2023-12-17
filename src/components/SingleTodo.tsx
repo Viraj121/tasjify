5a751724059612b5a73e986c8f9074def8d53ce6
Viraj Gavas <gavasviraj@gmail.com>
2023-12-17 22:36:18 +0530

    Tailwind css added

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import {AiFillDelete, AiFillEdit} from 'react-icons/ai'
-import {MdDone} from 'react-icons/md'
-import "./style.css"
+import React from "react";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "./style.css";
 
-type Props={
-    todo:Todo,
-    todos:Todo[],
-    setTodos:React.Dispatch<React.SetStateAction<Todo[]>>;
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
 
-const SingleTodo = ({todo, todos,setTodos }:Props) => {
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
   return (
-    <form className='todos__single'>
-        <span className='todo__single--text'>{todo.todo}</span>
-        <div>
-           <span className="icon">
-            <AiFillEdit/>
-            </span>
-            <span className="icon">
-            <AiFillDelete/>
-            </span> 
-            <span className="icon">
-            <MdDone />
-            </span>
-        </div>
-    </form>
-  )
-}
+    <form className="todos__single">
+      <span className="todo__single--text">{todo.todo}</span>
+      <div>
+        <span className="icon">
+          <AiFillEdit />
 
-export default SingleTodo
+          <AiFillDelete />
+
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default SingleTodo;
